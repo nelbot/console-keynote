268bbbf266281777ce8b136531ffa62ea40b6f9f
nelbot <nelson.cuadras@gmail.com>
2015-03-17 17:48:57 -0400

    Initial commit

---
+++ console_keynote.rb
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Console Keynote #
+
+# text will be centered, horizontally and vertically
+# next prompt will go to next slider
+# previous prompt will go to previous slide
