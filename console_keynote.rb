40e4cb8b4c9b4744e47500a89396b314703bc4ea
nelbot <nelson.cuadras@gmail.com>
2015-03-18 09:19:57 -0400

    Add Keynote and Slide class

--- console_keynote.rb
+++ console_keynote.rb
@@ -1,13 +1,42 @@
+require 'terminfo'
 
+class Keynote
+  def initialize
+    @all_slides = []
+  end
 
+  def add_slides(slides)
+    @all_slides.push(slides)
+  end
 
+  def presentation
+    puts @all_slides
+  end
+end
 
+class Slide
+attr_accessor :width
+attr_accessor :height
 
+  def initialize    
+    @width = width
+    @height = height
+    TermInfo.screen_size[0] = height
+    TermInfo.screen_size[1] = width
+  end
 
+  def show_me_the_money
+    puts @height
+    puts @width
+  end
+end
 
+# p TermInfo.screen_size
 
+# puts TermInfo.screen_size[0] 
 
-
+slide = Slide.new
+slide.show_me_the_money
 
 
 
@@ -22,3 +51,6 @@
 # text will be centered, horizontally and vertically
 # next prompt will go to next slider
 # previous prompt will go to previous slide
+
+# take vertical line number, subtract 1 line, and divide by 2. floating point will round down. 
+# take horizontal character numbers, subtract string.length, and divide by 2. float will round down. 
